c81d64832f7b1c3ae9584adb6a6b6f38a96fe615
unknown <ana.alandalib777@gmail.com>
2024-07-31 18:55:19 +0300

    SecondProject

--- MotionDetectionTrackingBackgroundSubtraction/DetectionTracking.py
+++ MotionDetectionTrackingBackgroundSubtraction/DetectionTracking.py
@@ -1,46 +1,50 @@
 import numpy as np
 import cv2 as cv
+from tracker import *
 
-cap = cv.VideoCapture("vtest.avi")
+result = cv.VideoWriter("out.mp4",cv.VideoWriter_fourcc(*'XVID')
+                        ,20, (250,250))
+cap = cv.VideoCapture("highway.mp4")
 
-width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
-height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+tracker = EuclideanDistTracker()
 
-fourcc = cv.VideoWriter_fourcc('X','V','I','D')
-out = cv.VideoWriter("output.avi", fourcc, 5.0, (1280,720))
+ObjectDetector = cv.createBackgroundSubtractorMOG2(100,50)
 
-ret, frame1 = cap.read()
-ret, frame2 = cap.read()
-
-while cap.isOpened():
+while cap.read():
+    ret, frame = cap.read()
     if ret:
-        diff = cv.absdiff(frame1,frame2)
-        gray = cv.cvtColor(diff,cv.COLOR_BGR2GRAY)
-        blur = cv.GaussianBlur(gray,(5,5),0)
-        _,thresh = cv.threshold(blur,20,255,cv.THRESH_BINARY)
-        dilated = cv.dilate(thresh, None, iterations=3)
+        height, width, _ = frame.shape
+        roi = frame[340: 720,500: 800] 
         
-        contours,_ = cv.findContours(dilated, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        mask = ObjectDetector.apply(roi)
+        _,mask = cv.threshold(mask,244,255,cv.THRESH_BINARY)
+        contours,_ = cv.findContours(mask,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE) 
+        detections = []
         
         for contour in contours:
-            x, y, w, h =  cv.boundingRect(contour)     
-            if cv.contourArea(contour) < 600:              
-                continue    
-            cv.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)              
-            cv.putText(frame1, "Status: {}".format('Movement'), (x, y)
-                        ,cv.FONT_HERSHEY_SIMPLEX,1, (0, 0, 255), 3)
+            area = cv.contourArea(contour)
+            if area>100:
+                x,y,w,h = cv.boundingRect(contour)
+                detections.append([x, y, w, h])              
+    
+        Bids = tracker.update(detections)
+        for Bid in Bids:
+            x,y,w,h,Id = Bid          
+            cv.putText(roi, str(Id), (x, y)
+                        ,cv.FONT_HERSHEY_PLAIN, 2, (255, 0, 0), 2)
+            cv.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 3)
             
-        image = cv.resize(frame1, (1280,720))
-        out.write(image)
-        cv.imshow("feed", frame1)
+        cv.imshow("roi", roi)
+        cv.imshow("Frame", frame)
+        cv.imshow("Mask", mask)
         
-        frame1 = frame2
-        ret, frame2 = cap.read()        
-        if cv.waitKey(40) == 27:
-            break     
-cv.destroyAllWindows()#close window
+        result.write(frame)             
+    
+    key = cv.waitKey(30)
+    if key == 27:
+        break        
 
-cap.release()#close camera
+result.release()
+cap.release()
 
-out.release()#close when write
-
+cv.destroyAllWindows()
